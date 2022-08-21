fbb5a801af08349271905e0c7034308951fc9453
Torcoroma <torcoromaarias@gmail.com>
2022-08-21 14:32:08 -0500

    Reform: resposive

--- src/components/ContainerApp.js
+++ src/components/ContainerApp.js
@@ -7,14 +7,17 @@ const ContainerApp = () => {
 
   return (
     <>
-      <div className="row justify-content-center">
+      <div className="row justify-content-center alert alert-success">
         <h1>
           Este es ContainerApp <br></br>
         </h1>
         {caracters.map((caracter) => {
           return (
             <>
-              <div className=" col-3  m-3 card p-2" key={caracter.name}>
+              <div
+                className=" col-ms-3 col-md-4 col-lg-3 m-3 card p-2"
+                key={caracter.id}
+              >
                 <img
                   src={caracter.images}
                   className="card-img-top "
@@ -23,7 +26,7 @@ const ContainerApp = () => {
                 <div className="card-body">
                   <h5 className="card-title">{caracter.title}</h5>
                   <p className="card-text text-danger">$ {caracter.price}</p>
-                  <a href="#" className="btn btn-primary">
+                  <a href="#" className="btn btn-success">
                     Agregar
                   </a>
                 </div>
