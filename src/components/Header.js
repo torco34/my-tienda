116fa4d2a6c26114dbd9eeecbf6328bedd01ce91
Torcoroma <torcoromaarias@gmail.com>
2022-08-21 16:10:30 -0500

    Reform: header listo

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import icmenu from "../assets/img/icon_menu.svg";
 import logo from "../assets/img/logo.jpg";
+import carrito from "../assets/img/carrito.jpeg";
 import "../assets/scss/Header.scss";
 
 const Header = () => {
@@ -14,33 +15,29 @@ const Header = () => {
           <ul>
             <Link to="/">Todo</Link>
             <Link to="/ropa">Ropa</Link>
-            {/* <Link to="/RecoveryPassword">Clothes</Link>
-            <Link to="/no">Checkout</Link>
-            <Link to="/createAccount">CreateAccont</Link>
-            <Link to="/cv">Toys</Link>
-            <Link to="/login">Contact</Link> */}
-
-            {/* <Input /> */}
+            <Link to="/#">Acesorios</Link>
+            <Link to="/no">Calzados</Link>
+            <Link to="/createAccount">Reloj</Link>
+            <Link to="/cv">Muebles</Link>
+            <Link to="/login">Comida</Link>
           </ul>
         </div>
 
-        {/* <div className="navbar-right">
+        <div className="navbar-right">
           <ul>
-            <li className="navbar-email" onClick={handleToggle}>
-              iniciar sesion
-            </li>
+            <li className="navbar-email">iniciar sesion</li>
             <li
               className="navbar-shopping-cart"
-              onClick={() => setToggleOrden(!toggleOrders)}
+              //   onClick={() => setToggleOrden(!toggleOrders)}
             >
               <img src={carrito} alt="shopping cart" />
-              {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
+              {/* {state.cart.length > 0 ? <div>{state.cart.length}</div> : null} */}
             </li>
           </ul>
         </div>
 
-        {toggle && <Menu />}
-        {toggleOrders && <MyOrder />} */}
+        {/* {toggle && <Menu />} */}
+        {/* {toggleOrders && <MyOrder />} */}
       </nav>
     </>
   );
